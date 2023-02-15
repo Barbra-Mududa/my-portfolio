fd1862ef032f389a6e12e868b73c5cd80266da94
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-02-15 18:13:05 +0300

    feat: create feature components

---
+++ src/components/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../navbar_footer/Navbar'
+
+function Home() {
+  return (
+    <div className="main-page">
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Home
