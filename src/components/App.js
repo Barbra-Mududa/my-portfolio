c1e4f2d61082d49745f35bc0c027f4170540e29a
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-02-15 18:16:38 +0300

    build: add routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
 import './App.css';
+import Home from './home/Home';
+import About from './about/About';
+import Resume from './resume/Resume';
+import Contact from './contact/Contact';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+        <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/" element={<Home />} />
+        </Routes>
+
     </div>
   );
 }
