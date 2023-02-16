cf1e4984e40f959086b9c5d5754d412f716d2d18
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-02-16 22:54:01 +0300

    feat: component structure complete

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -4,23 +4,19 @@ import './contact.css'
 function Contact() {
   return (
     <div className='homeContainer'>
-      <div className='contact-container'>
-        <div className='contact-me'>
-          <img id='phone' src='https://img.freepik.com/free-vector/phone_78370-560.jpg?w=2000'/>
-            <h2>Contact Me</h2>  
+      <form class="form">
+        <div className='contact-container'>
+          <div class="pageTitle title">
+              <img id='phone' src='https://img.freepik.com/free-vector/phone_78370-560.jpg?w=2000'/>
+              <h2>Contact Me</h2> 
+          </div>
+          <div class="secondaryTitle title">DISCUSS A PROJECT OR JUST GET IN TOUCH? MY INBOX IS OPEN</div>      
+            <input type="text" class="name formEntry" placeholder="Name" />
+            <input type="text" class="email formEntry" placeholder="Email"/>
+            <textarea class="message formEntry" placeholder="Message"></textarea>
+            <button class="submit formEntry" onclick="thanks()">Submit</button>  
         </div>
-        <div className='contact-message'>
-            <h3>DISCUSS A PROJECT OR JUST GET IN TOUCH? MY INBOX IS OPEN</h3>
-        </div>
-        <form class="form">
-          <input type="text" class="name formEntry" placeholder="Name" />
-          <input type="text" class="email formEntry" placeholder="Email"/>
-          <textarea class="message formEntry" placeholder="Message"></textarea>
-          <button class="submit formEntry" onclick="thanks()">Submit</button>
-        </form>
-        
-    </div>
-      
+      </form> 
     </div>
   )
 }
