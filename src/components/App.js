31a97ef22c3021c2c189ec7723efdbd70da3aafb
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-02-16 21:21:40 +0300

    perf: add cursor pointer for hover function

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import './App.css';
 import Home from './home/Home';
 import About from './about/About';
 import Resume from './resume/Resume';
 import Contact from './contact/Contact';
+import Navbar from './navbar_footer/Navbar';
 
 
 function App() {
-  
 
   return (
     <div className='app'>
+        
+        <Navbar/>
         <Routes>
             <Route path="/about" element={<About />} />
             <Route path="/resume" element={<Resume />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/" element={<Home />} />
         </Routes>
+        
         
     </div>
   );
