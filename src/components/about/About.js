234297e96da6abd1009e76aad00fd0d8e8598bc7
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-02-18 15:53:17 +0300

    feat: about component structure

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function About() {
   return (
-    <div>About</div>
+    <div className='homeContainer'>
+      <div>
+        <div>
+          <h2>About Me...</h2>
+          <h4>A young enthusiastic software developer who aspires to <br></br>
+              build interactive and practical platforms that will <br></br>
+              address current challenges.
+          </h4>
+        </div>
+        <div>
+          <h3>Education</h3>
+          <div>
+            {/* <Card/> */}
+          </div>
+        </div>
+        <div>
+          <h3>My Reads</h3>
+          <div>
+            <ul>
+              <li>
+                Leading Yourself
+                <p></p>
+              </li>
+              <li>
+                Teamwork Foundations
+                <p></p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='about-arrow'>
+        See My Projects<Link to='/projects'>
+          <i class="fa-sharp fa-solid fa-arrow-right" id='arrow'></i>
+          </Link> 
+      </div>
+      </div>
+    </div>
   )
 }
 
