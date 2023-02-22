6349bcf01e3dea890764e79543138d8124521667
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-02-23 00:24:33 +0300

    feat:compnent structure and functionality

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,42 +6,43 @@ import './about.css'
 function About() {
   return (
     <div className='aboutContainer'>
-      <div>
-        <div>
+      <div className='aboutSection'>
+        <div className='aboutHero'>
           <h2>About Me...</h2>
-          <h4>As a young and enthusiastic software developer, I have the ambition to create interactive and practical platforms <br></br>
-              that address current challenges. My goal is to help businesses and individuals solve their day-to-day problems with <br></br>
-              innovative solutions. My previous experience and skills have enabled me to build robust applications that are easy <br></br>
-              to use and highly efficient. With my expertise, I can provide you with the perfect tool for your business or project<br></br>
-              needs. Let me show you how my development skills can make a difference in your organization.
+          <h4>As a young and enthusiastic software developer, I have the ambition to create interactive and <br></br> practical platforms 
+              that address current challenges. My goal is to help businesses and individuals <br></br> solve their day-to-day problems with 
+              innovative solutions. My previous experience and skills have<br></br> enabled me to build robust applications that are easy 
+              to use and highly efficient. With my expertise,<br></br> I can provide you with the perfect tool for your business or project
+              needs. 
           </h4>
         </div>
-        <div>
+        <div className='education'>
           <h3>Education</h3>
-          <div className='card-container'>
             <Card />
-          </div>
         </div>
-        <div>
+        <div className='myReads'>
           <h3>My Reads</h3>
-          <div>
+          <div className='readList'>
             <ul>
-              <li>
-                Leading Yourself
-                <p></p>
+              <li><span>Leading Yourself</span>
+                <p>Leading yourself is an essential skill for success in any field. It involves developing self-awareness, setting and<br></br>
+                achieving goals, managing time effectivelyly,making decisions with confidence, and communicating  <br></br> 
+                effectively. It involves understanding your strengths and weaknesses, values and beliefs, and how you interact<br></br> 
+                with others. Moroever, it outlines  how to stay focused on what you want to achieve.
+                </p>
               </li>
-              <li>
-                Teamwork Foundations
-                <p></p>
+              <li><span>Teamwork Foundations</span>
+              <p>Teamwork Foundation is a set of principles and practices that help teams work together more effectively. It<br></br>
+               focuses on problem-solving, collaboration, conflict resolution, and team dynamics. By understanding the<br></br> importance of teamwork and how to foster it in the workplace, teams can become more productive and<br></br> successful.</p>
               </li>
             </ul>
           </div>
-        </div>
+        </div> 
         <div className='about-arrow'>
         See My Projects<Link to='/projects'>
           <i class="fa-sharp fa-solid fa-arrow-right" id='arrow'></i>
           </Link> 
-      </div>
+      </div> 
       </div>
     </div>
   )
