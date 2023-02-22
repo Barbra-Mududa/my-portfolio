abc04178d7bd054a551816d5899b010355d6ea72
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-02-22 20:00:01 +0300

    feat: about component structure

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import Card from './Card';
+import './about.css'
 
 function About() {
   return (
-    <div className='homeContainer'>
+    <div className='aboutContainer'>
       <div>
         <div>
           <h2>About Me...</h2>
-          <h4>A young enthusiastic software developer who aspires to <br></br>
-              build interactive and practical platforms that will <br></br>
-              address current challenges.
+          <h4>As a young and enthusiastic software developer, I have the ambition to create interactive and practical platforms <br></br>
+              that address current challenges. My goal is to help businesses and individuals solve their day-to-day problems with <br></br>
+              innovative solutions. My previous experience and skills have enabled me to build robust applications that are easy <br></br>
+              to use and highly efficient. With my expertise, I can provide you with the perfect tool for your business or project<br></br>
+              needs. Let me show you how my development skills can make a difference in your organization.
           </h4>
         </div>
         <div>
           <h3>Education</h3>
-          <div>
-            {/* <Card/> */}
+          <div className='card-container'>
+            <Card />
           </div>
         </div>
         <div>
