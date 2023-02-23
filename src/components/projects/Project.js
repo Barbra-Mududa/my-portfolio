3d3759147208c20f0364f98149107bf36bd28572
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-02-24 01:44:44 +0300

    feat: project component structure

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,44 +1,34 @@
 import React from 'react'
 import './project.css'
+import ProjectCard from './ProjectCard'
 
 function Project() {
   return (
     <div className='projectContainer'>
       <div className='project-section'>
-        <div className='project-heading'>
-          <h2>My Projects</h2>
-        </div>
-        <div className='project-cards'>
-          <h3>Projects</h3>
+        <div className='projects'>
+          <h2>My Projects & Collaborations</h2>
+          <ProjectCard/>
         </div>
         <div className='technologies'>
-          <h3>TECHNOLOGIES I USED TO BUILD STUFF</h3>
+          <h3>Technologies I Use</h3>
           <div className='tech-icons'>
              <img id='tech1' 
                 src='https://www.datocms-assets.com/45470/1631110818-logo-react-js.png' 
                 alt=''
               />
-              <img id='tech1' 
-                src='https://cdn-icons-png.flaticon.com/512/5968/5968267.png' 
+              <img id='tech1' className='three-icons'
+                src='https://www.freepnglogos.com/uploads/html5-logo-png/html5-logo-devextreme-multi-purpose-controls-html-javascript-3.png' 
                 alt=''
               />
               <img id='tech1' 
-                src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png' 
-                alt=''
-              />
-              <img id='tech1' 
-                src='https://1000logos.net/wp-content/uploads/2020/09/JavaScript-Logo.png' 
-                alt=''
-              />
-              <img id='tech1' 
-                src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/1200px-Bootstrap_logo.svg.png' 
+                src='https://i.postimg.cc/W4JpB1B4/image-5-3.jpg' 
                 alt=''
               />
               <img id='tech1' 
                 src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png' 
                 alt=''
               />
-            <div>
               <img id='tech' 
                 src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ruby_logo.svg/1200px-Ruby_logo.svg.png' 
                 alt=''
@@ -51,15 +41,11 @@ function Project() {
                 src='https://d1.awsstatic.com/asset-repository/products/amazon-rds/1024px-MySQL.ff87215b43fd7292af172e2a5d9b844217262571.png'
                 alt=''
               />
-            </div>
           </div>
         </div>
-        <div className='stacks'>
+        {/* <div className='stacks'>
           <h3> My Stacks</h3>
-          {/* <div>
-
-          </div> */}
-        </div>
+        </div> */}
       </div>
     </div>
   )
