16f2f931b8ed4eaf9544449fd812deabfa0e1ed4
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-02-24 01:43:24 +0300

    feat: navbar responsiveness

--- src/components/navbar_footer/Navbar.js
+++ src/components/navbar_footer/Navbar.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { DarkModeSwitch } from 'react-toggle-dark-mode';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./navbar.css";
 import {useState, useEffect} from 'react';
+import { DarkModeSwitch } from "react-toggle-dark-mode";
 
-import "./navbar.css"
 
-
-function Navbar() {
+function Navbar({ user, setUser}) {
   const [isDarkMode, setDarkMode] = useState(false);
-  
-  useEffect(() => {
+
+    useEffect(() => {
     const body = document.querySelector('body');
     if (isDarkMode) {
       body.classList.add('dark-mode');
@@ -22,27 +21,56 @@ function Navbar() {
     setDarkMode(checked);
   };
 
-  return ( 
-    <div className='navbar'>
-      <div className='nav-section'>
-            <div className='logo'>
-                <NavLink exact to= "/" id='logos'>Barbra Mududa</NavLink>
-            </div>
-            <nav>
-                <NavLink to= "/about" id='links'>About</NavLink>
-                <NavLink to= "/projects" id='links'>Project</NavLink>
-                <NavLink to= "/resume" id='links'>Resume</NavLink>
-                <NavLink to= "/contact" id='links'>Contact</NavLink>
-            </nav>
-            <DarkModeSwitch
-                style={{ marginBottom: '2rem' }}
-                checked={isDarkMode}
-                onChange={toggleDarkMode}
-                size={120}
-            />
+  const menu = () => {
+    const links = document.querySelector('.middle-nav')
+    const menu = document.querySelector('.menu')
+    links.classList.toggle('active')
+    menu.classList.toggle('clicked')
+  }
+
+  const aLink = () => {
+    const links = document.querySelector('.middle-nav')
+    const menu = document.querySelector('.menu')
+    links.classList.remove('active')
+    menu.classList.remove('clicked')
+  }
+
+  const myStyle= {
+    marginTop: '0rem',
+    position : "relative",
+    right: "0.4rem"
+  }
+
+  return (
+    <nav>
+      <div className="nav-header">
+        <NavLink to="/" id="header" onClick={aLink}>
+          Barbra Mududa
+        </NavLink>
       </div>
-    </div>
-  )
+
+      <div className="middle-nav">
+      <div className="container1-nav">     
+        <NavLink to="/about" className="navlink" onClick={aLink}>About</NavLink>
+        <NavLink to="/projects" className="navlink" onClick={aLink}>Project</NavLink>
+        <NavLink to="/resume" className="navlink" onClick={aLink}>Resume</NavLink>
+        <NavLink to="/contact" className="navlink" onClick={aLink}>Contact </NavLink>
+        <DarkModeSwitch
+        style={myStyle}
+        checked={isDarkMode}
+        onChange={toggleDarkMode}
+        size={50}
+       /> 
+      </div>
+      </div>
+          
+        <div className="menu" onClick={menu}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
