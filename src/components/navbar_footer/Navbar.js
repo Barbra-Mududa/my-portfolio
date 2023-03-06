c1a4b44db78ad7e6872831cacb1cca9e9fcbfd30
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-03-07 00:43:32 +0300

    Co-authored-by: Kibet Immanuel Ngeno <iamkibeh@users.noreply.github.com>

--- src/components/navbar_footer/Navbar.js
+++ src/components/navbar_footer/Navbar.js
@@ -1,19 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./navbar.css";
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 import { DarkModeSwitch } from "react-toggle-dark-mode";
 
-
-function Navbar({ user, setUser}) {
+function Navbar({ user, setUser }) {
   const [isDarkMode, setDarkMode] = useState(false);
 
-    useEffect(() => {
-    const body = document.querySelector('body');
+  useEffect(() => {
+    const body = document.querySelector("body");
     if (isDarkMode) {
-      body.classList.add('dark-mode');
+      body.classList.add("dark-mode");
     } else {
-      body.classList.remove('dark-mode');
+      body.classList.remove("dark-mode");
     }
   }, [isDarkMode]);
 
@@ -22,24 +21,24 @@ function Navbar({ user, setUser}) {
   };
 
   const menu = () => {
-    const links = document.querySelector('.middle-nav')
-    const menu = document.querySelector('.menu')
-    links.classList.toggle('active')
-    menu.classList.toggle('clicked')
-  }
+    const links = document.querySelector(".middle-nav");
+    const menu = document.querySelector(".menu");
+    links.classList.toggle("active");
+    menu.classList.toggle("clicked");
+  };
 
   const aLink = () => {
-    const links = document.querySelector('.middle-nav')
-    const menu = document.querySelector('.menu')
-    links.classList.remove('active')
-    menu.classList.remove('clicked')
-  }
+    const links = document.querySelector(".middle-nav");
+    const menu = document.querySelector(".menu");
+    links.classList.remove("active");
+    menu.classList.remove("clicked");
+  };
 
-  const myStyle= {
-    marginTop: '0rem',
-    position : "relative",
-    right: "0.4rem"
-  }
+  const myStyle = {
+    marginTop: "0rem",
+    position: "relative",
+    right: "0.4rem",
+  };
 
   return (
     <nav>
@@ -50,25 +49,33 @@ function Navbar({ user, setUser}) {
       </div>
 
       <div className="middle-nav">
-      <div className="container1-nav">     
-        <NavLink to="/about" className="navlink" onClick={aLink}>About</NavLink>
-        <NavLink to="/projects" className="navlink" onClick={aLink}>Project</NavLink>
-        <NavLink to="/resume" className="navlink" onClick={aLink}>Resume</NavLink>
-        <NavLink to="/contact" className="navlink" onClick={aLink}>Contact </NavLink>
-        <DarkModeSwitch
-        style={myStyle}
-        checked={isDarkMode}
-        onChange={toggleDarkMode}
-        size={50}
-       /> 
-      </div>
-      </div>
-          
-        <div className="menu" onClick={menu}>
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
+        <div className="container1-nav">
+          <NavLink to="/about" className="navlink" onClick={aLink}>
+            About
+          </NavLink>
+          <NavLink to="/projects" className="navlink" onClick={aLink}>
+            Project
+          </NavLink>
+          <NavLink to="/resume" className="navlink" onClick={aLink}>
+            Resume
+          </NavLink>
+          <NavLink to="/contact" className="navlink" onClick={aLink}>
+            Contact{" "}
+          </NavLink>
+          <DarkModeSwitch
+            style={myStyle}
+            checked={isDarkMode}
+            onChange={toggleDarkMode}
+            size={50}
+          />
         </div>
+      </div>
+
+      <div className="menu" onClick={menu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
     </nav>
   );
 }
