fb69b747ba005410a2a86dd132709aef422e67da
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-03-30 13:50:52 +0300

    feat: add a footer component

---
+++ src/components/navbar_footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './footer.css'
+
+function Footer() {
+  return (
+        <div className='footer'>© Copyright 2023 Barbra Mududa</div>
+  )
+}
+
+export default Footer
